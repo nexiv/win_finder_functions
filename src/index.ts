aaf2fc90c1b891f26cece1f254628cf175969e7a
Nemanja Prascevic <nexiv@Nemanjas-MacBook-Air.local>
2025-11-02 13:44:06 +0100

    feature: export all functions

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-// import * as logger from "firebase-functions/logger";
 import { initializeApp } from "firebase-admin/app";
 import { setGlobalOptions } from "firebase-functions";
 import { updateOdds } from "./functions/update_odds";
+import { updateResults } from "./functions/update_results";
+import { updateStandings } from "./functions/update_standings";
+import { cleanDatabase } from "./functions/clean_database";
 
 initializeApp();
 setGlobalOptions({ region: 'europe-west1', timeoutSeconds: 300 });
 
 exports.updateOdds = updateOdds;
+exports.update_results = updateResults;
+exports.updateStandings = updateStandings;
+exports.cleanDatabase = cleanDatabase;
 
 
